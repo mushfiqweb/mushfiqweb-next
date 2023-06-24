1f70a3bee172803910a3015eb99fc0752b729b1e
Mushfiq <hello@mushfiqweb.com>
2023-06-24 21:55:20 +0600

    init

---
+++ data/siteData.ts
@@ -0,0 +1,17 @@
+const siteData = {
+  title: 'Mushfiqur\'s Blog',
+  author: 'Mushfiqur\'s Blog',
+  headerTitle: 'Mushfiqur\'s Blog',
+  profileUrl: '/me.gif',
+  headerDescription: 'Full Stack Developer, Tech Geek, Audiophile, Cinephile, and Lifelong Learner!',
+  footerText: '© All rights reserved',
+  language: 'en-us',
+  locale: 'en-US',
+  socialBanner: 'https://nextjs-notion-blog-starter.vercel.app/preview/1.jpg',
+  github: 'https://github.com/mushfiqweb',
+  twitter: 'https://twitter.com/mushfiqweb',
+  websiteUrl: 'https://mushfiqweb.com',
+  linkedin: 'https://github.com/tuanphungcz/nextjs-notion-blog-starter'
+};
+
+export default siteData;
